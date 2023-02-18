ae0ba40bfa0046b022bf22729c1226e489700139
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-02-18 01:45:12 -0700

    Convert to Options API & start fetch

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<script setup>
+<script>
 	import IAmError from './components/IAmError.vue';
 	import CommunityIcon from './components/icons/IconCommunity.vue';
 
-	import { ref } from 'vue';
-	let message = ref('Hello World!');
+	export default {
+		components: {
+			IAmError,
+			CommunityIcon
+		},
+		data() {
+			return {
+				message: 'Hello World!',
+				pokemonData: []
+			}
+		},
+		methods: {
+			fetchPokemon() {
+				console.log('bleh');
+				/* fetch('')
+				.then(response => response.json())
+				.then(data => this.pokemonData = data.results); */
+			}
+		}
+	};
 </script>
 
 <template>
@@ -12,6 +30,7 @@
 		<h1>{{ message }}</h1>
 		<button @click="message = 'Goodbye World!'" class="button my-4">Click me</button>
 		<IAmError friend="Bagu" />
+		<button @click="fetchPokemon()" class="button my-4">Get First 10 Pokemon</button>
 	</main>
 </template>
 

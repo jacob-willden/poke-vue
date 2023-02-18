443c80ee29fa3ae787def615c066d699d5e5c622
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-02-18 12:34:10 -0700

    Add basic Pokémon fetch

--- src/App.vue
+++ src/App.vue
@@ -15,10 +15,11 @@
 		},
 		methods: {
 			fetchPokemon() {
-				console.log('bleh');
-				/* fetch('')
+				fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
 				.then(response => response.json())
-				.then(data => this.pokemonData = data.results); */
+				.then(data => {
+					this.pokemonData = data.results;
+				});
 			}
 		}
 	};
@@ -31,6 +32,9 @@
 		<button @click="message = 'Goodbye World!'" class="button my-4">Click me</button>
 		<IAmError friend="Bagu" />
 		<button @click="fetchPokemon()" class="button my-4">Get First 10 Pokemon</button>
+		<ul>
+			<li v-for="pokemon in pokemonData" :key="pokemon.url">{{ pokemon.name }}</li> <!-- If v-for is used on the ul element, each li will have its own ul element -->
+		</ul>
 	</main>
 </template>
 

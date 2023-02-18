cf859482b3f13893b3d1d419bf432cd679f3d208
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-02-18 00:43:48 -0700

    Experiment with the default components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+	import IAmError from './components/IAmError.vue';
+	import CommunityIcon from './components/icons/IconCommunity.vue';
+
+	import { ref } from 'vue';
+	let message = ref('Hello World!');
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<CommunityIcon />
+		<h1>{{ message }}</h1>
+		<button @click="message = 'Goodbye World!'" class="button my-4">Click me</button>
+		<IAmError friend="Bagu" />
+	</main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+	h1 {
+		font-size: 2rem;
+	}
+	.my-4 {
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
 </style>
---
+++ src/components/IAmError.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+	friend: {
+		type: String,
+		required: true
+	}
+});
+</script>
+
+<template>
+	<h2>I am Error. Go meet {{ friend }}.</h2>
+</template>
